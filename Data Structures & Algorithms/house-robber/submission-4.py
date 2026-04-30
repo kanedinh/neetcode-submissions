6ca070dbc8c0f1ca5f300d014c564661d4a48770
Kane <inhxuankhang@gmail.com>
2026-04-30 12:42:51 +0700

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # DP Top-Down
+        # n = len(nums)
+        # dp = [-1] * n
+        # def dfs(i):
+        #     if i >= n:
+        #         return 0
+        #     if dp[i] != -1:
+        #         return dp[i]
+        #     dp[i] = max(dfs(i + 1), nums[i] + dfs(i + 2))
+        #     return dp[i]
+        # return dfs(0)
+        # [1, 2, 3, 1]
+        # [1, 2, 4, 4]
+        # DP Bottom-Up
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp[n - 1]
